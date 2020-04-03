f641ca9ed678abf3e4d4ce43de7a2f8f283467f7
Arleneq <49526016+Arleneq@users.noreply.github.com>
2020-04-03 11:34:27 +0800

    Add files via upload

--- music-web/script/main.js
+++ music-web/script/main.js
@@ -1,3 +1,65 @@
+var btn = document.getElementsByClassName("icon")[0];
+var myMusic = document.getElementById("myMusic");
+var txt = document.getElementById("txt");
+var con = document.getElementsByClassName("content")[0];
+var mark = true;
+
+btn.onclick = function(){
+    if (mark){
+        this.className += " rotate";
+        myMusic.play();
+        mark = false;
+    } else {
+        this.className = "icon";
+        myMusic.pause();
+        mark = true;
+    }
+}
+btn.ondblclick = function(){
+    if (mark){
+        this.className += " rotate";
+        myMusic.play();
+        mark = false;
+    } else {
+        this.className = "icon";
+        myMusic.load();
+        mark = true;
+        con.style.top = 0+"px";
+    }
+}
+
+var lrc = txt.value;
+var lrcArr = lrc.split("[");
+var html = "";
+for (var i = 0 ; i < lrcArr.length ; i++){
+    var arr = lrcArr[i].split("]");
+    var time = arr[0].split(".");
+    var timer = time[0].split(":");
+    var ms = timer[0] * 60 + timer[1] * 1;
+    var text = arr[1];
+    if (text){
+        html += "<p id=" + ms +">"+ text +"</p>";
+    }
+
+    con.innerHTML = html;
+}
+
+var num = 0;
+var op = con.getElementsByTagName("p");
+myMusic.addEventListener("timeupdate",function(){
+    var curTime = parseInt(this.currentTime);
+    if(document.getElementById(curTime)){
+        for (var i = 0 ; i < op.length ; i++){
+            op[i].style.cssText = "font-size: 15px;";
+        }
+        document.getElementById(curTime).style.cssText = "background: linear-gradient(-3deg,rgb(120, 81, 228) 0%,#474afa 100%);-webkit-background-clip: text;color: transparent;font-size: 20px;";
+        if(op[3 + num].id == curTime){
+            con.style.top = -35 * num + "px";
+            num++;
+        }
+    }
+})
+
 window.onload=init;		
 function init() {			
     var o=document.getElementById('box');			
@@ -28,67 +90,4 @@ function init() {
         }			
     }		
 }
-
-
-var btn = document.getElementsByClassName("icon")[0];
-var myMusic = document.getElementById("myMusic");
-var txt = document.getElementById("txt");
-var con = document.getElementsByClassName("content")[0];
-var mark = true;
-
-btn.onclick = function(){
-    if (mark){
-        this.className += " rotate";
-        myMusic.play();
-        mark = false;
-    } else {
-        this.className = "icon";
-        myMusic.pause();
-        mark = true;
-    }
-}
-btn.ondblclick = function(){
-    if (mark){
-        this.className += " rotate";
-        myMusic.play();
-        mark = false;
-    } else {
-        this.className = "icon";
-        myMusic.load();
-        mark = true;
-    }
-}
-
-var lrc = txt.innerHTML;
-var lrcArr = lrc.split("[");
-var html = "";
-for (var i = 0 ; i < lrcArr.length ; i++){
-    var arr = lrcArr[i].split("]");
-    var time = arr[0].split(".");
-    var timer = time[0].split(":");
-    var ms = timer[0] * 60 + timer[1] * 1
-    var text = arr[1];
-    if (text){
-        html += "<p id=" + ms +">"+ text +"</p>";
-    }
-
-    console.log(html);
-    con.innerHTML = html;
-}
-
-var num = 0;
-var op = con.getElementsByTagName("p");
-myMusic.addEventListener("timeupdate",function(){
-    var curTime = parseInt(this.currentTime)
-    if(document.getElementById(curTime)){
-        for (var i = 0 ; i < op.length ; i++){
-            op[i].style.cssText = "font-size: 15px;"
-        }
-        document.getElementById(curTime).style.cssText = "background: linear-gradient(-3deg,rgb(120, 81, 228) 0%,#474afa 100%);-webkit-background-clip: text;color: transparent;font-size: 20px;position: relative;"
-        if(op[7 + num].id == curTime){
-            con.style.top = -20 * num + "px";
-            num++;
-        }
-    }
-})
 
